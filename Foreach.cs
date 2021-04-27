ef24d8dc9d7e0b89be1e2b31d84b43b618d633ee
s-iozaki <reighous@gmail.com>
2021-04-27 23:16:43 +0900

    day5

---
+++ Foreach.cs
@@ -0,0 +1,18 @@
+using System;
+
+class ForeachDemo{
+    static void Main(){
+        int sum = 0;
+        int[,] nums = new int[3,5];
+
+        for(int i = 0;i<3;i++)
+        for(int j = 0;j<5;j++)
+        nums[i,j] = (i+1)*(j+1);
+
+        foreach(int x in nums){
+        Console.WriteLine("Value is :" + x);
+        sum += x;
+        }
+        Console.WriteLine("Sumnation :"+ sum);
+    }
+}
